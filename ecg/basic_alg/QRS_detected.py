937f576a7f7f356f06a347f3351f60a5345e51fa
zhoudengshi <zhoudengshi@vivest.cn>
2022-08-10 17:50:52 +0800

    init file

---
+++ ecg/basic_alg/QRS_detected.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：ecg-algorithm
+@File    ：QRS_detected.py
+@Author  ：DFQX
+@Date    ：2022/8/10 17:48
+@Description:
+"""
+
+
+def r_wave_detected():
+    pass
+
+
+if __name__ == '__main__':
+    pass
